17427505210a5e7ee1fcce14c03769ff759ec0fe
Adelshin <ai.adelshin@stalmail.org>
2023-04-07 11:26:38 +0500

    спринт 1: инкремент 5

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Orendev/shortener
 go 1.19
 
 require (
+	github.com/caarlos0/env/v8 v8.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -2,24 +2,39 @@ package configs
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v8"
+	"log"
 )
 
-var options Configs = Configs{}
+var cfg Configs = Configs{}
+var addr string
+var baseURL string
 
 type Configs struct {
-	Addr    string
-	Host    string
-	Port    string
-	BaseURL string
-}
-
-func init() {
-	flag.StringVar(&options.Addr, "a", "", "Адрес запуска сервера localhost:8080")
-	flag.StringVar(&options.BaseURL, "b", "http://localhost:8080", "Базовый URL http://localhost:8080")
-	flag.StringVar(&options.Port, "p", "8080", "Порт 8080")
+	Addr    string `env:"SERVER_ADDRESS"`
+	Host    string `env:"HOST"`
+	Port    string `env:"PORT"`
+	BaseURL string `env:"BASE_URL"`
 }
 
 func New() (*Configs, error) {
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	flag.StringVar(&addr, "a", "localhost:8080", "Адрес запуска сервера localhost:8080")
+	flag.StringVar(&baseURL, "b", "http://localhost:8080", "Базовый URL http://localhost:8080")
 	flag.Parse()
-	return &options, nil
+
+	if len(cfg.Addr) == 0 {
+		cfg.Addr = addr
+	}
+	if len(cfg.BaseURL) == 0 {
+		cfg.BaseURL = baseURL
+	}
+
+	return &cfg, nil
 }
