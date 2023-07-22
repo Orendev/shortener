9cc2b8680d427ca4ebfbcdb84651cdc07aff6cad
Adelshin <ai.adelshin@stalmail.org>
2023-07-23 01:28:18 +0500

    спринт 7: инкремент 18

--- internal/random/strn.go
+++ internal/random/strn.go
@@ -8,13 +8,13 @@ import (
 
 // Strn получим рандомное число
 func Strn(n int) string {
-	rand.Seed(time.Now().UnixNano())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	chars := []rune(alphabet + strings.ToLower(alphabet))
 
 	var b strings.Builder
 	for i := 0; i < n; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
+		b.WriteRune(chars[r.Intn(len(chars))])
 	}
 
 	return b.String()
