4e7a7a50a919bf7d1a571f33a509685b8174d9f5
Adelshin <ai.adelshin@stalmail.org>
2023-08-01 23:00:29 +0500

    спринт 8: инкремент 20

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	_ "net/http/pprof"
 
 	"github.com/Orendev/shortener/internal/app"
 	"github.com/Orendev/shortener/internal/config"
 	"github.com/Orendev/shortener/internal/logger"
+)
+
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
 )
 
 func main() {
@@ -19,6 +26,10 @@ func main() {
 	if err := logger.NewLogger(cfg.Log.FlagLogLevel); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 
 	app.Run(cfg)
 }
